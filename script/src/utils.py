2850ace5bcfc49e40260e713e54f5b9b62705ee6
deemoe <90408077+deemoe404@users.noreply.github.com>
2024-10-30 20:41:20 +0800

    添加 user_commits 函数
    用于根据用户名获取该用户的公开 commit

--- script/src/utils.py
+++ script/src/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+from time import sleep
+
 import requests
 import re
 
@@ -24,3 +27,31 @@ def commit_timezone(username: str, repo: str, commit_hash: str):
         return matches[0][0][-5:]
     else:
         return -1
+
+
+def user_commits(username: str, interval_days: float = 365, sleep_time: float = 0.1):
+    """
+    Fetches the commits made by a user within a specified interval.
+
+    Args:
+        username (str): The GitHub username.
+        interval_days (float): The number of days to look back from today. Defaults to 365.
+        sleep_time (float): The time to sleep between requests to avoid hitting rate limits. Defaults to 1 second.
+
+    Returns:
+        list: A list of commits made by the user within the specified interval.
+    """
+    url_template = 'https://api.github.com/search/commits?q=author:{author}+committer-date:{start}..{end}'
+
+    end_date = datetime.date(datetime.now())
+    start_date = end_date - timedelta(days=interval_days)
+    commits = []
+    url = url_template.format(author=username, start=start_date, end=end_date)
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        commits.extend(data.get('items', []))
+        url = response.links.get('next', {}).get('url')
+        sleep(sleep_time)
+    return commits
