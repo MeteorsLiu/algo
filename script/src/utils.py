45069671c7e43bd1d5345366c2d28bde1a91a137
deemoe <90408077+deemoe404@users.noreply.github.com>
2024-11-02 04:44:45 +0800

    移动函数到 utils_online

--- script/src/utils.py
+++ script/src/utils.py
@@ -1,14 +1,18 @@
-from datetime import datetime, timedelta
-from time import sleep
-from geopy.geocoders import Nominatim
-from collections import Counter
-
-import numpy as np
-import requests
 import re
+from datetime import datetime, timedelta
 
+import country_converter as coco
+import pycountry
+import pytz
+import tldextract
+from fast_langdetect import detect_multilingual
+from pytz import country_timezones
 
-def commit_timezone(repo_fullname: str, commit_hash: str):
+timezone_countries = {}
+for country, timezones in country_timezones.items():
+    for timezone in timezones:
+        timezone_countries.setdefault(timezone, set()).add(country)
+
 language_countries_LUT = {
     "zh": "China",
     "en": "United States",
@@ -106,110 +110,55 @@ language_countries_LUT = {
 
 def language_countries(lang_code: str) -> str | None:
     """
-    Fetches the timezone information from a specific commit on GitHub.
-
     依照查找表（LUT）返回语言对应的国家。
     Args:
-        repo_fullname (str): The repository full name.
-        commit_hash (str): The commit hash.
-
         lang_code: 语言代码。
     Returns:
-        str: The timezone offset in the format `+/-HHMM` if exactly one match is found.
-        int: -1 if no matches or multiple matches are found.
         语言对应的国家。若没有找到对应的国家则返回 None。
     """
-    url = f"https://github.com/{repo_fullname}/commit/{commit_hash}.patch"
-    response = requests.get(url)
     return language_countries_LUT.get(lang_code, None)
 
-    pattern = r"(Date:\s.*?([+-]\d{4}))"
-    matches = [(match.group(), match.start()) for match in re.finditer(pattern, response.text)]
 
-    if len(matches) == 1:  # TODO: add position filtering
-        return matches[0][0][-5:]
-    else:
-        return -1
-
-
-def user_commits(username: str, interval_days: float = 365, sleep_time: float = 0.1):
 def text_lang(text: str):
     """
-    Fetches the commits made by a user within a specified interval.
-
     获取文本的语言分布。
     Args:
-        username (str): The GitHub username.
-        interval_days (float): The number of days to look back from today. Defaults to 365.
-        sleep_time (float): The time to sleep between requests to avoid hitting rate limits. Defaults to 1 second.
-
         text: 文本内容。
     Returns:
-        list: A list of commits made by the user within the specified interval.
         语言分布列表，若未找到语言则返回空列表。
     """
-    url_template = 'https://api.github.com/search/commits?q=author:{author}+committer-date:{start}..{end}'
-
-    end_date = datetime.date(datetime.now())
-    start_date = end_date - timedelta(days=interval_days)
-    commits = []
-    url = url_template.format(author=username, start=start_date, end=end_date)
-
-    while url:
-        response = requests.get(url)
-        data = response.json()
-        commits.extend(data.get('items', []))
-        url = response.links.get('next', {}).get('url')
-        sleep(sleep_time)
-    return commits
     if not text:
         return None
     newtext = re.sub(r'\n', ' ', text)
     return detect_multilingual(newtext)
 
 
-def user_location(username: str):
 def timezone_nation(timezone_offset: str):
     """
-    Fetches the geographical information of a user from GitHub.
-
     根据时区偏移值获取可能的国家。
     Args:
-        username (str): The GitHub username.
-
         timezone_offset: 时区偏移值，格式为 `+/-HHMM`。
     Returns:
-        dict: The geographical information state by the user.
         可能的国家列表。
     """
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url)
     hours_offset = int(timezone_offset[:3])
     minutes_offset = int(timezone_offset[0] + timezone_offset[3:])
 
-    if response.status_code == 200:
-        return response.json().get('location')
-    return None
     utc_offset = timedelta(hours=hours_offset, minutes=minutes_offset)
     now = datetime.now(pytz.utc)
 
     all_timezones = {tz.zone for tz in map(pytz.timezone, pytz.all_timezones_set) if
                      now.astimezone(tz).utcoffset() == utc_offset}
 
-def location_nation(location_name: str, max_results: int = 32):
-    """
-    Fetches the probable countries for a given location name using geocoding.
     countries = []
     for tz in all_timezones:
         country_set = timezone_countries.get(tz, set())
         if country_set:
             countries.extend(country_set)
 
-    Args:
-        location_name (str): The name of the location to geocode.
-        max_results (int): The maximum number of results to return. Defaults to 32.
     cc = coco.CountryConverter()
     return cc.convert(names=list(set(countries)), to='name_short')
+
 
 def email_nation(email: str):
     """
@@ -217,33 +166,13 @@ def email_nation(email: str):
     Args:
         email: 电子邮件地址。
     Returns:
-        list: A list of dictionaries containing country names and their probabilities.
         可能的国家名称。
     """
-    geolocator = Nominatim(user_agent="algo-demo-geo")
-    locations = geolocator.geocode(location_name, exactly_one=False, limit=max_results)
-
-    country_counts = Counter()
-
-    if locations:
-        for loc in locations:
-            address = loc.raw.get("display_name", "")
-            address_parts = address.split(", ")
-            country = address_parts[-1] if address_parts else "Unknown"
-            country_counts[country] += 1
-
-    counts = np.array(list(country_counts.values()), dtype=np.float32)
-    softmax_probs = np.exp(counts) / np.sum(np.exp(counts))
     extracted = tldextract.extract(email)
     tld = extracted.suffix
 
-    result = [
-        {"country": country, "probability": prob}
-        for country, prob in zip(country_counts.keys(), softmax_probs)
-    ]
     result = None
     if len(tld) == 2:
         result = pycountry.countries.get(alpha_2=tld.upper())
 
-    return result
     return result.name if result else None
