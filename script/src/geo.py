824bf1f21d0872f45555edded3d571f940cecea8
deemoe <90408077+deemoe404@users.noreply.github.com>
2024-11-06 16:54:24 +0800

    Update geo.py

--- script/src/geo.py
+++ script/src/geo.py
@@ -1,77 +1,145 @@
 import utils
 import utils_online
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def get_user_nation(username: str, token: str = None):
-    """
-    获取用户的国家信息。
-    Args:
-        username: GitHub 用户名。
-        token: GitHub API 访问令牌。
-    Returns:
-        用户的国家信息 {"nation": str, "probability": float}。
-    """
-    # 尝试从用户自述信息中获取国家信息
-    user_info = utils_online.user_info(username)
+
+def from_profile(username: str, token: str):
+    user_info = utils_online.user_info(username, token)
     if user_info.get('location') is not None:
         location_nation = utils_online.location_nation(user_info.get('location'))
         if location_nation is not None:
             return {'nation': location_nation, "probability": 1}
         else:
             print(f"没有找到“{user_info.get('location')}”对应的国家。")
+            return None
     else:
         print(f"{username} 没有填写位置信息。")
+        return None
 
-    # 尝试从用户仓库的 README 语言中推断国家信息（排除英文）
+
+def from_readme(username: str, token: str, concurrency: int = 10):
+    repos = utils_online.user_repos(username)
+    if not repos or len(repos) == 0:
+        print(f"{username} 没有仓库。")
+        return None
+
+    def fetch_readme_lang_distribution(repo):
+        readme = utils_online.repo_readme(repo_fullname=repo, token=token)
+        if readme is not None:
+            return utils.text_lang(readme)
+        return None
+
     readme_langs = {}
     entry_count = 0
-    for repo in utils_online.user_repos(username):
-        readme = utils_online.repo_readme(repo_fullname=repo['full_name'], token=token)
-        if readme is None: continue
+    with ThreadPoolExecutor(max_workers=concurrency) as executor:
+        futures = {executor.submit(fetch_readme_lang_distribution, repo): repo for repo in repos}
+        for future in as_completed(futures):
+            lang_distribution = future.result()
+            if lang_distribution is None:
+                continue
 
-        lang_distribution = utils.text_lang(readme)
-        if lang_distribution is None: continue
-
-        entry_count += 1
-        for item in lang_distribution:
-            readme_langs[item['lang']] = readme_langs.get(item['lang'], 0) + item['score']
+            entry_count += 1
+            for item in lang_distribution:
+                readme_langs[item['lang']] = readme_langs.get(item['lang'], 0) + item['score']
 
     if entry_count > 0:
-        readme_langs.pop('en', None)  # 排除英文
-        readme_nation_probability = max(readme_langs.values()) / entry_count
-        readme_nation_name = utils.language_countries(max(readme_langs, key=readme_langs.get))
-        if readme_nation_probability > 0.2 and readme_nation_name is not None:
-            return {'nation': readme_nation_name, "probability": readme_nation_probability}
-        else:
-            print(f"README 置信概率低 {readme_langs}。")
+        readme_langs.pop('en', None)
+        if readme_langs:
+            readme_nation_probability = max(readme_langs.values()) / entry_count
+            readme_nation_name = utils.language_countries(max(readme_langs, key=readme_langs.get))
+            if readme_nation_name is not None:
+                return {'nation': readme_nation_name, "probability": readme_nation_probability}
+            else:
+                print("语言未知")
+                return None
+    else:
+        print(f"{username} 的仓库没有可用的 README 信息。")
+        return None
 
-    # 尝试从用户提交记录中推断国家信息
-    # TODO: 确定应该返回时区偏移量还是国家列表
-    timezones = {}
-    for commit in utils_online.user_commits(username, maximum=8):
-        timezone = utils_online.commit_timezone(commit['repository']['full_name'], commit['sha'])
-        if timezone != -1:
-            timezones[timezone] = timezones.get(timezone, 0) + 1
-    if not timezones:
+
+def from_relationship(username: str, token: str, concurrency: int = 10):
+    mutual_followings = utils_online.user_mutual_follows(username, token)
+    if not mutual_followings:
+        print(f"{username} 没有共同关注的 GitHub 用户。")
+        return None
+
+    def fetch_user_nation(user):
+        _profile = from_profile(user, token)
+        if _profile is not None:
+            return _profile
+        else:
+            return from_readme(user, token)
+
+    nations = {}
+    valid_data = 0
+    with ThreadPoolExecutor(max_workers=concurrency) as executor:
+        futures = {executor.submit(fetch_user_nation, user): user for user in mutual_followings}
+        for future in as_completed(futures):
+            nation = future.result()
+            if nation is not None and nation.get('nation') is not None:
+                try:
+                    nations[nation['nation']] = nations.get(nation['nation'], 0) + nation['probability']
+                    valid_data += 1
+                except Exception as e:
+                    print(f"Error: {e}")
+
+    if nations:
+        most_likely_nation = max(nations, key=nations.get)
+        most_likely_nation_probability = nations[most_likely_nation] / valid_data
+        return {'nation': most_likely_nation, "probability": most_likely_nation_probability}
+    else:
+        print(f"没有找到共同关注列表中 GitHub 用户的国家信息。")
+        return None
+
+
+def from_commits(username: str, token: str, concurrency: int = 10):
+    commits = utils_online.user_commits(username, token, 8)
+    if not commits or len(commits) == 0:
         print(f"{username} 没有提交记录。")
         return None
+
+    def fetch_commit_timezone(commit):
+        return utils_online.commit_timezone(commit['repository']['full_name'], commit['sha'])
+
+    timezones = {}
+    with ThreadPoolExecutor(max_workers=concurrency) as executor:
+        futures = {executor.submit(fetch_commit_timezone, commit): commit for commit in commits}
+        for future in as_completed(futures):
+            timezone = future.result()
+            if timezone is not None:
+                timezones[timezone] = timezones.get(timezone, 0) + 1
+
+    if not timezones:
+        print(f"{username} 的提交记录没有有效的时区信息。")
+        return None
     else:
+        # TODO: 确定应该返回时区偏移量还是国家列表
         most_often_timezone = max(timezones, key=timezones.get)
         most_often_nation = utils.timezone_nation(most_often_timezone)
         most_often_nation_probability = timezones[most_often_timezone] / sum(timezones.values())
-        return {'nation': most_often_timezone, "probability": most_often_nation_probability}
+        return {'nation': most_often_nation, "probability": most_often_nation_probability}
 
 
-def last_resort(username: str, token: str = None):
-    # 尝试从共同关注列表中推断国家信息
-    mutual_followings = utils_online.user_mutual_follows(username, token)
-    if mutual_followings:
-        nations = []
-        for user in mutual_followings:
-            nation = get_user_nation(user, token)
-            if nation is not None:
-                nations.append(nation['nation'])
-        if nations:
-            return {'nation': max(set(nations), key=nations.count), "probability": 1}
-        else:
-            print(f"没有找到共同关注列表中 GitHub 用户的国家信息。")
+def get_nation(username: str, token: str, concurrency: int = 32):
+    profile = from_profile(username, token)
+    if profile is not None:
+        return profile
+
+    readme = from_readme(username, token, concurrency)
+    if readme is not None:
+        return readme
+
+    relationship = from_relationship(username, token, concurrency)
+    if relationship is not None:
+        return relationship
+
+    commits = from_commits(username, token, concurrency)
+    if commits is not None:
+        return commits
+
+    print(f"无法确定 {username} 的国家信息。")
+    return None
+
+
+print(get_nation("deemoe404", "xxxxxxx"))
